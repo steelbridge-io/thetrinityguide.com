f2dc762c335b581de3a6e7418409d833bc1c823e
steelbridge-io <chris@steelbridge.io>
2020-12-31 21:47:29 -0800

    Updated testimonial slider height across all devices

--- lib/testimonials.php
+++ lib/testimonials.php
@@ -4,25 +4,28 @@
   {
     if(is_front_page() || is_home()) {
       echo '
+            <div class="container-fluid wide">
+            <div class="container">
+            <h1 class="text-center">What Our Customers Say</h1>
             <div id="carouselExampleControls" class="carousel slide carousel-fade" data-bs-ride="carousel">
               <div class="carousel-inner">
                 <div class="carousel-item active">
                 <div class="container narrow">
-                <p class="text-center">“Alex is a professional at whatever he does. Fishing and hunting being number one. If there’s a fish to be caught, Alex will find it! He’s a pleasure to be around and knows how to make everyone’s day one to remember. Don’t hesitate to give him a call, and get out on the water with him. You won’t regret it.”</p>
+                <p class="text-center lead">“Alex is a professional at whatever he does. Fishing and hunting being number one. If there’s a fish to be caught, Alex will find it! He’s a pleasure to be around and knows how to make everyone’s day one to remember. Don’t hesitate to give him a call, and get out on the water with him. You won’t regret it.”</p>
                     <p class="text-center fw-bolder">Aaron Martin<br>
                     Fisheries Biologist, Trinity River</p>
                 </div>
                 </div>
                 
                 <div class="carousel-item">
-                  <div class="container narow">
-                  <p class="text-center">"We love fishing with Alex. He works really hard to find the fish and we\'ve caught a lot of them on our trips. What makes it really great is his beautiful comfortable boat (you can get out of the sun) and the great conversation. He\'s a serious fisherman, but a lot of fun too. We have a blast"</p>
+                  <div class="container narrow">
+                  <p class="text-center lead">"We love fishing with Alex. He works really hard to find the fish and we\'ve caught a lot of them on our trips. What makes it really great is his beautiful comfortable boat (you can get out of the sun) and the great conversation. He\'s a serious fisherman, but a lot of fun too. We have a blast"</p>
                   </div>
                 </div>
                 
                 <div class="carousel-item">
-                <div class="container narow">
-                <p class="text-center">"Alex is an amazing guide - we hit our limit on Kokanee and Browns each time we’ve been out with him and had a hell of a time doing it. Great conversations, lots of fun, and plenty of fish to bring home and smoke. The Trinity Guide Company boat was definitely the envy of the lake. Highly recommended to book a day with him, you won’t regret it."</p>
+                <div class="container narrow">
+                <p class="text-center lead">"Alex is an amazing guide - we hit our limit on Kokanee and Browns each time we’ve been out with him and had a hell of a time doing it. Great conversations, lots of fun, and plenty of fish to bring home and smoke. The Trinity Guide Company boat was definitely the envy of the lake. Highly recommended to book a day with him, you won’t regret it."</p>
                 <p class="text-center fw-bolder">
                 Matt RyderSan Francisco, CA
                 </p>
@@ -37,6 +40,8 @@
             <span class="carousel-control-next-icon" aria-hidden="true"><i class="fas fa-2x fa-chevron-right"></i></span>
             <span class="visually-hidden">Next</span>
             </a>
+            </div>
+            </div>
             </div>
            ';
     }
