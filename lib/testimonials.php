2cc4f6e2c13cc8444ca9114489e3459c4e8c9715
steelbridge-io <chris@steelbridge.io>
2020-12-31 20:37:26 -0800

    Added .container class

--- lib/testimonials.php
+++ lib/testimonials.php
@@ -7,7 +7,7 @@
             <div id="carouselExampleControls" class="carousel slide carousel-fade" data-bs-ride="carousel">
               <div class="carousel-inner">
                 <div class="carousel-item active">
-                <div class="container">
+                <div class="container narrow">
                 <p class="text-center">“Alex is a professional at whatever he does. Fishing and hunting being number one. If there’s a fish to be caught, Alex will find it! He’s a pleasure to be around and knows how to make everyone’s day one to remember. Don’t hesitate to give him a call, and get out on the water with him. You won’t regret it.”</p>
                     <p class="text-center fw-bolder">Aaron Martin<br>
                     Fisheries Biologist, Trinity River</p>
@@ -15,13 +15,13 @@
                 </div>
                 
                 <div class="carousel-item">
-                  <div class="container">
+                  <div class="container narow">
                   <p class="text-center">"We love fishing with Alex. He works really hard to find the fish and we\'ve caught a lot of them on our trips. What makes it really great is his beautiful comfortable boat (you can get out of the sun) and the great conversation. He\'s a serious fisherman, but a lot of fun too. We have a blast"</p>
                   </div>
                 </div>
                 
                 <div class="carousel-item">
-                <div class="container">
+                <div class="container narow">
                 <p class="text-center">"Alex is an amazing guide - we hit our limit on Kokanee and Browns each time we’ve been out with him and had a hell of a time doing it. Great conversations, lots of fun, and plenty of fish to bring home and smoke. The Trinity Guide Company boat was definitely the envy of the lake. Highly recommended to book a day with him, you won’t regret it."</p>
                 <p class="text-center fw-bolder">
                 Matt RyderSan Francisco, CA
